34eee000e1fe7895613914920663e05d182950c7
chrisptrader <insulfion@gmail.com>
2026-02-28 22:27:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,12 @@ def webhook():
     signal = data.get("signal")
     pair = data.get("pair")
     price = float(f"{float(data.get('price')):.5f}")
-    atr = float(f"{float(data.get('atr')):.5f}")
+    atr_value = data.get("atr")
 
+if atr_value is None:
+    atr = 0.0005
+else:
+    atr = float(atr_value
     print(f"ALERT RECEIVED: {signal} {pair} Price: {price} ATR: {atr}")
     
     return {"status": "received"}, 200
