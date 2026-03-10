f52c12dbf942899f3d15eb99275273128fb537dc
chrisptrader <insulfion@gmail.com>
2026-03-09 23:09:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+Enable desktop notifications for Gmail.
+   OK  No thanks
+1 of 1,880
+Bit
+Inbox
+
+Chris Pena <insulfion@gmail.com>
+11:07 PM (0 minutes ago)
+to me
+
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -9,16 +19,17 @@ def home():
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.get_json()
-    
+
     signal = data.get("signal")
     pair = data.get("pair")
     price = float(f"{float(data.get('price')):.5f}")
     atr_value = data.get("atr")
 
-if atr_value is None:
-    atr = 0.0005
-else:
-    atr = float(atr_value)
+    if atr_value is None:
+        atr = 0.0005
+    else:
+        atr = float(atr_value)
+
     print(f"ALERT RECEIVED: {signal} {pair} Price: {price} ATR: {atr}")
-    
+
     return {"status": "received"}, 200
