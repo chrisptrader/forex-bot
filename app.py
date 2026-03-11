d1de4f1036748bcad34fbdccd9c369e28187ffff
chrisptrader <insulfion@gmail.com>
2026-03-11 00:05:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -63,5 +63,8 @@ def webhook():
         print(f"OANDA ERROR: {e}")
         return jsonify({"error": str(e)}), 500
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
